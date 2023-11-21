d892efffc9bff056257a199f47da1b2788639672
tianyu-z <zhangtianyu.pro@gmail.com>
2023-11-21 14:39:01 -0500

    update PPO against AFN

--- Pretrained_PPO_play_against_AFN.py
+++ Pretrained_PPO_play_against_AFN.py
@@ -11,6 +11,7 @@ import os
 import torch
 import pickle
 import numpy as np
+from tqdm import tqdm
 
 # Load or train your PPO model
 PPO_model_path = (
@@ -23,7 +24,11 @@ AFN_connect4 = Connect4Env()
 state_dims, action_dims = get_env_dims(AFN_connect4)
 
 # Initialize the environment
-PPO_connect4 = ConnectFourEnv(opponent=NN, render_mode="human", main_player_name="AFN")
+verbose = False
+render_mode = "human" if verbose else "rgb_array"
+PPO_connect4 = ConnectFourEnv(
+    opponent=NN, render_mode=render_mode, main_player_name="AFN"
+)
 
 # Initialize the AFN
 AFN = AZResNet(state_dims, action_dims)
@@ -46,66 +51,107 @@ if AFN_model_path is not None:
 AFN.to(DEVICE)
 AFN.eval()
 
-# Game loop
-obs, _ = PPO_connect4.reset()
-AFN_connect4.reset()
-AFN_first = False
-AFN_connect4.render()
-while not AFN_connect4.done:
-    if AFN_connect4.curr_player == Player.ONE:
-        if AFN_first:
-            AFN_action = AFN.sample_actions(AFN_connect4, 0).item()
-            AFN_connect4.step(AFN_action)
-            AFN_connect4.render()
+
+def play_once(verbose=verbose):
+    # Game loop
+    obs, _ = PPO_connect4.reset()
+    AFN_connect4.reset()
+    AFN_first = False
+    if verbose:
+        AFN_connect4.render()
+    while not AFN_connect4.done:
+        if AFN_connect4.curr_player == Player.ONE:
+            if AFN_first:
+                AFN_action = AFN.sample_actions(AFN_connect4, 0).item()
+                AFN_connect4.step(AFN_action)
+                if verbose:
+                    AFN_connect4.render()
+            else:
+                try:
+                    obs, rewards, dones, truncated, info = PPO_connect4.step(AFN_action)
+                except:
+                    AFN_action = None
+                    obs, rewards, dones, truncated, info = PPO_connect4.step(AFN_action)
+                valid_PPO_moves = np.where(AFN_connect4.get_masks())[0]
+                PPO_action = info["oppoent_action"]
+                assert PPO_action in valid_PPO_moves, "Invalid move! Please try again."
+                AFN_connect4.step(PPO_action)
+                if verbose:
+                    AFN_connect4.render()
+                    PPO_connect4.render()
+                if truncated or dones:
+                    obs, _ = PPO_connect4.reset()
+                    AFN_connect4.reset()
+                    if verbose:
+                        AFN_connect4.render()
         else:
-            try:
-                obs, rewards, dones, truncated, info = PPO_connect4.step(AFN_action)
-            except:
-                AFN_action = None
-                obs, rewards, dones, truncated, info = PPO_connect4.step(AFN_action)
-            valid_PPO_moves = np.where(AFN_connect4.get_masks())[0]
-            PPO_action = info["oppoent_action"]
-            assert PPO_action in valid_PPO_moves, "Invalid move! Please try again."
-            AFN_connect4.step(PPO_action)
-            AFN_connect4.render()
-            PPO_connect4.render()
-            if truncated or dones:
-                obs, _ = PPO_connect4.reset()
-                AFN_connect4.reset()
-                AFN_connect4.render()
-    else:
-        if AFN_first:
-            try:
-                obs, rewards, dones, truncated, info = PPO_connect4.step(AFN_action)
-            except:
-                AFN_action = None
-                obs, rewards, dones, truncated, info = PPO_connect4.step(AFN_action)
-            valid_PPO_moves = np.where(AFN_connect4.get_masks())[0]
-            PPO_action = info["oppoent_action"]
-            assert PPO_action in valid_PPO_moves, "Invalid move! Please try again."
-            AFN_connect4.step(PPO_action)
-            AFN_connect4.render()
-            PPO_connect4.render()
-            if truncated or dones:
-                obs, _ = PPO_connect4.reset()
-                AFN_connect4.reset()
-                AFN_connect4.render()
+            if AFN_first:
+                try:
+                    obs, rewards, dones, truncated, info = PPO_connect4.step(AFN_action)
+                except:
+                    AFN_action = None
+                    obs, rewards, dones, truncated, info = PPO_connect4.step(AFN_action)
+                valid_PPO_moves = np.where(AFN_connect4.get_masks())[0]
+                PPO_action = info["oppoent_action"]
+                assert PPO_action in valid_PPO_moves, "Invalid move! Please try again."
+                AFN_connect4.step(PPO_action)
+                if verbose:
+                    AFN_connect4.render()
+                    PPO_connect4.render()
+                if truncated or dones:
+                    obs, _ = PPO_connect4.reset()
+                    if verbose:
+                        AFN_connect4.reset()
+                        AFN_connect4.render()
+            else:
+                AFN_action = AFN.sample_actions(AFN_connect4, 0).item()
+                AFN_connect4.step(AFN_action)
+                if verbose:
+                    AFN_connect4.render()
+    if verbose:
+        AFN_connect4.render()
+    isAFNwin = 0
+    if not AFN_first:
+        if AFN_connect4.outcome == Outcome.WIN_P2:
+            if verbose:
+                print("AFN won!")
+            isAFNwin = 1
+        elif AFN_connect4.outcome == Outcome.WIN_P1:
+            if verbose:
+                print("PPO won!")
+            isAFNwin = -1
         else:
-            AFN_action = AFN.sample_actions(AFN_connect4, 0).item()
-            AFN_connect4.step(AFN_action)
-            AFN_connect4.render()
-AFN_connect4.render()
-if not AFN_first:
-    if AFN_connect4.outcome == Outcome.WIN_P2:
-        print("AFN won!")
-    elif AFN_connect4.outcome == Outcome.WIN_P1:
-        print("PPO won!")
+            if verbose:
+                print("It's a draw!")
+            isAFNwin = 0
     else:
-        print("It's a draw!")
-else:
-    if AFN_connect4.outcome == Outcome.WIN_P1:
-        print("AFN won!")
-    elif AFN_connect4.outcome == Outcome.WIN_P2:
-        print("PPO won!")
-    else:
-        print("It's a draw!")
+        if AFN_connect4.outcome == Outcome.WIN_P1:
+            if verbose:
+                print("AFN won!")
+            isAFNwin = 1
+        elif AFN_connect4.outcome == Outcome.WIN_P2:
+            if verbose:
+                print("PPO won!")
+            isAFNwin = -1
+        else:
+            if verbose:
+                print("It's a draw!")
+            isAFNwin = 0
+    return isAFNwin
+
+
+if __name__ == "__main__":
+    result = {"win": 0, "lose": 0, "draw": 0}
+    for i in tqdm(range(1000)):
+        try:
+            isAFNwin = play_once()
+        except:
+            continue
+        if isAFNwin == 1:
+            result["win"] += 1
+        elif isAFNwin == -1:
+            result["lose"] += 1
+        elif isAFNwin == 0:
+            result["draw"] += 1
+        else:
+            raise Exception("Invalid result!")
